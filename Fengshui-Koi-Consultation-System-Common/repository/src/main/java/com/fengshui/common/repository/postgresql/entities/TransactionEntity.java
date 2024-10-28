e3e9ad7f810af37a1b9c03e477f997450bff7bc7
Truong-Dio <85091699+Truong-Dio@users.noreply.github.com>
2024-10-29 04:21:04 +0700

    Update final TransactionEntity

--- Fengshui-Koi-Consultation-System-Common/repository/src/main/java/com/fengshui/common/repository/postgresql/entities/TransactionEntity.java
+++ Fengshui-Koi-Consultation-System-Common/repository/src/main/java/com/fengshui/common/repository/postgresql/entities/TransactionEntity.java
@@ -1,40 +1,35 @@
-// package com.fengshui.common.repository.postgresql.entities;
-// <<<<<<< truongpn
-// import jakarta.persistence.*;
-// import lombok.*;
+ package com.fengshui.common.repository.postgresql.entities;
+ import jakarta.persistence.*;
+ import lombok.*;
 
-// import java.time.LocalDateTime;
-// import java.util.UUID;
-// @Getter
-// @Setter
-// @NoArgsConstructor
-// @AllArgsConstructor
-// @Builder
-// @Entity
-// @Table(name = "transaction")
-// public class TransactionEntity {
-//     @jakarta.persistence.Id
-//     @GeneratedValue(strategy = GenerationType.UUID)
-//     @Column (name = "transaction_id")
-//     private UUID Id;
+ import java.time.LocalDateTime;
+ import java.util.UUID;
+ @Getter
+ @Setter
+ @NoArgsConstructor
+ @AllArgsConstructor
+ @Builder
+ @Entity
+ @Table(name = "transaction")
+ public class TransactionEntity {
+     @jakarta.persistence.Id
+     @GeneratedValue(strategy = GenerationType.UUID)
+     @Column(name = "transaction_id")
+     private UUID Id;
 
-//     @Column(name = "created_at", nullable = false, updatable = false)
-//     private LocalDateTime createdAt;
+     @Column(name = "created_at", nullable = false, updatable = false)
+     private LocalDateTime createdAt;
 
-//     @OneToOne
-//     @JoinColumn (name = "app_user_id", referencedColumnName = "app_user_id")
-//     private AppUser UserId;
+     @OneToOne
+     @JoinColumn(name = "app_user_id", referencedColumnName = "app_user_id")
+     private AppUserEntity UserId;
 
-//     @Column (name = "ads_package")
-//     private String adsPackage;
+     @Column(name = "ads_package")
+     private String adsPackage;
 
-//     @Column (name = "price")
-//     private double price;
+     @Column(name = "price")
+     private double price;
 
-//     @Column(name = "deleted", nullable = false)
-//     private boolean deleted;
-// =======
-
-// public class TransactionEntity {
-// >>>>>>> main
-// }
+     @Column(name = "deleted", nullable = false)
+     private boolean deleted;
+ }
--- Fengshui-Koi-Consultation-System-Common/repository/src/main/java/com/fengshui/common/repository/postgresql/mapper/TransactionMapper.java
+++ Fengshui-Koi-Consultation-System-Common/repository/src/main/java/com/fengshui/common/repository/postgresql/mapper/TransactionMapper.java
@@ -1,24 +1,44 @@
 package com.fengshui.common.repository.postgresql.mapper;
 
+import com.fengshui.common.repository.postgresql.IAppUserRepository;
 import com.fengshui.common.repository.postgresql.dto.TransactionDTO;
+import com.fengshui.common.repository.postgresql.entities.AppUser;
+import com.fengshui.common.repository.postgresql.entities.AppUserEntity;
 import com.fengshui.common.repository.postgresql.entities.TransactionEntity;
 
 import java.time.LocalDateTime;
 
 public class TransactionMapper {
+    private final IAppUserRepository appUserRepository;
+    public TransactionMapper(IAppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
     public static TransactionDTO toDTO(TransactionEntity entity){
         if (entity == null) {return null;
     }
     return TransactionDTO.builder()
             .id(entity.getId())
             .createdAt(LocalDateTime.parse(entity.getCreatedAt().toString()))
-            .userId(entity.getUserId())
+            .appUser(entity.getAppUser().getId())
             .adsPackage(entity.getAdsPackage())
+            .price(entity.getPrice())
             .build();
 }
 
 
-public static TransationEntity toEntity(TransactionDTO dto) {
-    if (dto == null) return null;
+public static TransactionEntity toEntity(TransactionDTO dto, IAppUserRepository appUserRepository) {
+    if (dto == null) {
+        return null;
+    }
+    AppUserEntity getAppUser = appUserRepository.findById(dto.getAppUser())
+            .orElseThrow(() -> new RuntimeException("User not found"));
+
+    return TransactionEntity.builder()
+            .Id(dto.getId())
+            .createdAt(dto.getCreatedAt())
+            .appUser(getAppUser)
+            .adsPackage(dto.getAdsPackage())
+            .price(dto.getPrice())
+            .build();
+    }
 }
-return TransactionEntity.builder()
